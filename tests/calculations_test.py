93686b0f00e290ac9e3d92f3b8102d21adb26793
Nina Jakovljević <60719465+njakov@users.noreply.github.com>
2025-09-03 13:28:14 +0000

    Fix imports.

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -1,3 +1,15 @@
+# System Modules
+import sys
+import os
+
+# Installed Modules
+# None
+
+# Project Modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from calculations import area_of_circle, get_nth_fibonacci   # noqa: E402
+
+
 def test_get_nth_fibonacci_ten():
    """Test with n=10."""
    # Arrange
